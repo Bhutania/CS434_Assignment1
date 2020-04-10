8d5ca452a031a1c89f7ced9d425ac7aae12b5fa3
Arnav Bhutani <bhutania@flip3.engr.oregonstate.edu>
2020-04-10 16:44:16 -0700

    added testing data to model and renamed variables

--- q1_2.py
+++ q1_2.py
@@ -1,23 +1,32 @@
 import numpy as np
 import os
 
-h_test_data = os.getcwd() + '/housing_train.csv'
+h_training_data = os.getcwd() + '/housing_train.csv'
+h_test_data = os.getcwd() + '/housing_test.csv'
 
-raw_x = np.genfromtxt(h_test_data, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), delimiter=',',)
-Y = np.genfromtxt(h_test_data, usecols=(-1),  delimiter=',')
-X = np.insert(raw_x, 0, 1, axis=1)
-W = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, Y))
+raw_training_x = np.genfromtxt(h_training_data, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), delimiter=',')
+training_y = np.genfromtxt(h_training_data, usecols=(-1),  delimiter=',')
+training_x = np.insert(raw_training_x, 0, 1, axis=1)
+weights = np.dot(np.linalg.inv(np.dot(training_x.T, training_x)), np.dot(training_x.T, training_y))
 
+test_y = np.genfromtxt(h_test_data, usecols=(-1), delimiter=',')
+raw_test_x = np.genfromtxt(h_test_data, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), delimiter=',')
+test_x = np.insert(raw_test_x, 0, 1, axis=1)
 
-print(X)
-print(Y)
-print(W)
+training_err=0
 
-err=0
+for x in range(0, training_y.size):
+    training_err+=(training_y[x]-np.inner(training_x[x], weights))**2
 
-for x in range(0, Y.size):
-    err+=(Y[x]-np.inner(X[x], W))**2
+training_err=(training_err/training_y.size)
 
-err=(err/Y.size)
+print(training_err)
 
-print(err)
+testing_err = 0
+
+for x in range(0, test_y.size):
+    testing_err+=(test_y[x]-np.inner(test_x[x], weights))**2
+
+testing_err=(testing_err/test_y.size)
+
+print(testing_err)
--- q1_3.py
+++ q1_3.py
@@ -1,17 +1,30 @@
 import numpy as np
 import os
 
-h_test_data = os.getcwd() + '/housing_train.csv'
+h_training_data = os.getcwd() + '/housing_train.csv'
+h_test_data = os.getcwd() + '/housing_test.csv'
 
-X = np.genfromtxt(h_test_data, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), delimiter=',',)
-Y = np.genfromtxt(h_test_data, usecols=(-1),  delimiter=',')
-W = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, Y))
+training_x = np.genfromtxt(h_training_data, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), delimiter=',')
+training_y = np.genfromtxt(h_training_data, usecols=(-1),  delimiter=',')
+weights = np.dot(np.linalg.inv(np.dot(training_x.T, training_x)), np.dot(training_x.T, training_y))
 
-err=0
+test_y = np.genfromtxt(h_test_data, usecols=(-1), delimiter=',')
+test_x = np.genfromtxt(h_test_data, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), delimiter=',')
 
-for x in range(0, Y.size):
-    err+=(Y[x]-np.inner(X[x], W))**2
+training_err=0
 
-err=(err/Y.size)
+for x in range(0, training_y.size):
+    training_err+=(training_y[x]-np.inner(training_x[x], weights))**2
 
-print(err)
+training_err=(training_err/training_y.size)
+
+print(training_err)
+
+testing_err = 0
+
+for x in range(0, test_y.size):
+    testing_err+=(test_y[x]-np.inner(test_x[x], weights))**2
+
+testing_err=(testing_err/test_y.size)
+
+print(testing_err)
