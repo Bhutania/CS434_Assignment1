4b0c0e017970d6d622621c69f264c03324d59ddd
jtparks <parksju@oregonstate.edu>
2020-04-11 22:13:25 -0700

    Updated plots to plot actual accuracies

--- q2_3.py
+++ q2_3.py
@@ -3,21 +3,19 @@ import numpy as np
 import os
 import sys
 
-# args = sys.argv
-# if(args[1] == "usps_train.csv" and args[2] == "usps_test.csv"):
-# u_train_data = args[1]
-u_train_data = "usps_train.csv"
-# u_test_data = args[2]
-u_test_data = "usps_test.csv"
-# else:
-#     print("Incorrect files passed, exiting...")
-#     quit()
+args = sys.argv
+if(args[1] == "usps_train.csv" and args[2] == "usps_test.csv"):
+    u_train_data = args[1]
+    u_test_data = args[2]
+else:
+    print("Incorrect files passed, exiting...")
+    quit()
 
 np.seterr(all='raise')
 learning_factor = .01
-lambdas = sys.argv[3][1:len(sys.argv[3])-1].split(",")
-train_list = []
-test_list = []
+lambdas = sys.argv[3][1:][:-1].split(",")
+train_correct = [0]*len(lambdas)
+test_correct = [0]*len(lambdas)
 
 train_x = np.genfromtxt(u_train_data, usecols=range(256), delimiter=',')
 train_y = np.genfromtxt(u_train_data, usecols=(-1),  delimiter=',')
@@ -28,7 +26,8 @@ test_x = np.insert(test_x, 0, 1, axis=1)
 test_y = np.genfromtxt(u_test_data, usecols=(-1),  delimiter=',')
 weights = np.zeros(train_x.shape[1])
 
-for k in lambdas:
+for k in range(len(lambdas)):
+    # Create the gradient and weight vector
     for i in range(1000):
         gradient = np.zeros(train_x.shape[1])
         for j in range(train_x.shape[0]):
@@ -36,43 +35,35 @@ for k in lambdas:
             gradient = np.add(gradient,  ((y_hat-train_y[j]) * train_x[j]))
         gradient = np.add(gradient, (learning_factor)*(np.linalg.norm(weights)))
         weights = np.subtract(weights, (learning_factor * gradient))
-    
-    # Get the training error for each weight vector
-    train_err=0
-    for x in range(0, train_y.size):
-        train_err+=(train_y[x]-np.inner(train_x[x], weights))**2
-    train_err=(train_err/train_y.size)
-    print("train: " + str(train_err))
-    # Keep track of them
-    train_list.append(train_err)
 
-    # Get the testing error for each weight vector
-    test_err=0
-    for x in range(0, test_y.size):
-        test_err+=(test_y[x]-np.inner(test_x[x], weights))**2
-    test_err=(test_err/test_y.size)
-    print("test: " + str(test_err))
-    # Keep track of them
-    test_list.append(test_err)
+    # Get the number of correct predictions for the training data for kth lambda
+    for j in range(train_x.shape[0]):
+        if np.dot(weights.T, train_x[j]) >= 0. and train_y[j] == 1:
+            train_correct[k]+=1
+        elif np.dot(weights.T, train_x[j]) < 0. and train_y[j] == 0:
+            train_correct[k]+=1
+    train_correct[k] = (train_correct[k]/train_x.shape[0])*100
+    print("Training Accuracy for " + str(k) + "lambda: " + str(train_correct[k]))
 
-correct = 0
+    # Get the number of correct predictions for the test data for kth lambda
+    for j in range(test_x.shape[0]):
+        if np.dot(weights.T, test_x[j]) >= 0. and test_y[j] == 1:
+            test_correct[k]+=1
+        elif np.dot(weights.T, test_x[j]) < 0. and test_y[j] == 0:
+            test_correct[k]+=1
+    test_correct[k] = (test_correct[k]/test_x.shape[0])*100
+    print("Testing Accuracy for " + str(k) + "th lambda: " + str(test_correct[k]))
 
-for i in range(train_x.shape[0]):
-    if np.dot(weights.T, train_x[i]) >= 0. and train_y[i] == 1:
-        correct+=1
-    elif np.dot(weights.T, train_x[i]) < 0. and train_y[i] == 0:
-        correct+=1
 
-print(correct/train_x.shape[0])
-
-plt.plot(lambdas, train_list)
+# Plot the two correct lists vs the given lambdas
+plt.plot(lambdas, train_correct)
 plt.xlabel("lambdas")
 plt.xticks(lambdas)
 plt.ylabel("Training Accuracy")
 plt.savefig("TrainingAcc.png")
 plt.clf()
 
-plt.plot(lambdas, test_list)
+plt.plot(lambdas, test_correct)
 plt.xlabel("lambdas")
 plt.xticks(lambdas)
 plt.ylabel("Testing Accuracy")
